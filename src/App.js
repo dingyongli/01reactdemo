c32ab59e249d878534050bb3688d8cbb5a87e35d
Chui <70761763@qq.com>
2020-04-15 14:52:10 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/**
+ * 应用根组件
+ * 
+ */
+
+ import React,{Component} from 'react'
+
+export default class App extends Component{
+    
+    render(){
+        return(
+            <div>app</div>
+        )
+    }
+
+}
